581fdddbb842000d574b43716332fd1380d89d5c
abdullah-miah <mdabdullaha708@gmail.com>
2022-08-21 02:11:43 +0600

    full form markup done

--- src/Components/Form/Registration.js
+++ src/Components/Form/Registration.js
@@ -21,6 +21,20 @@ const Registration = () => {
                 id:"",
                 govtId:"",
                 gurdian:"",
+                GurdianName:"",
+                email:"",
+                emargencyNO:"",
+                address:"",
+                country: "India",
+                state: "",
+                city:"",
+                pincode:"",
+                occupation:"",
+                religion:"",
+                marrid:"",
+                blood:"",
+                nationality:"India"
+                
             }} onSubmit={fromdata}>
     
             <Form>
@@ -64,14 +78,90 @@ const Registration = () => {
                 </div>
                 {/* Contact Details */}
 
-                <div>
+               <div className='flex' >
+                <h1>Contact Details</h1>
+                <br/>
+               <div>
                    <label>Gurdian Details</label> 
                    <Field className="input"  name="gurdian" as ="select">
                     <option value='Mother'>Mother</option>
                     <option value='Father'>Father</option>
                     <option value='Sister'>Sister</option>
                 </Field>
+                <Field className="input" placeholder="Enter Gurdain Name" name="GurdianName" type ="text"></Field>
                 </div>
+                <div>
+                    <label>Email</label>
+                    <Field className="input" placeholder="Enter Email" name="email" type ="email"></Field>
+                    <label>Emergencey Contact Number</label>
+                    <Field className="input" placeholder="Enter Emargency No" name="emargencyNO" type ="text"></Field>
+                </div>
+               </div>
+               {/* Address Details */}
+               <div>
+                <h1>Address Details</h1>
+                <div className='flex'>
+                    <div>
+                        <label>Address</label>
+                        <Field className="input" placeholder="Enter Address" name="address" type ="text"></Field>
+                        <br/>
+                        <label>Country</label>
+                        <Field className="input" placeholder="Enter Counter" name="country" type ="text"></Field>
+                        <br/>
+                    </div>
+                    <div>
+                        <label>State</label>
+                        <Field className="input"  name="state" as="select">
+                        <option value='State"'>State</option>
+                    <option value='Kolkata"'>Kolkata</option>
+                    <option value='Kolkata"'>Kolkata</option>
+                    <option value='Kolkata"'>Kolkata</option>
+                </Field>
+                <label>City</label>
+                <Field className="input"  name="city" as="select">
+                <option value='City"'>City</option>
+                    <option value='Kolkata"'>Kolkata</option>
+                    <option value='Kolkata"'>Kolkata</option>
+                    <option value='Kolkata"'>Kolkata</option>
+                </Field>
+                <br/>
+                <label>Pincode</label>
+                <Field className="input" placeholder="Enter Pincode" name="pincode" type ="text"></Field>
+                    </div>
+                </div>
+
+               </div>
+               <div>
+                {/* other Details */}
+                <h1>Other Details</h1>
+                <label>Occupation</label>
+                <Field className="input" placeholder="Enter Occupation" name="occupation" type ="text"></Field>
+                <label>Religion</label>
+                <Field className="input"  name="religion" as="select">
+                    <option value='Islam"'>Islam</option>
+                    <option value='Hindu"'>Hindu</option>
+                    <option value='khristan"'>khristan</option>
+                    <option value='Other"'>Ohter</option>
+                </Field>
+                <label>Merital Status</label>
+                <Field className="input"  name="marrid" as="select">
+                    <option value='Marrid"'>Marrid</option>
+                    <option value='Unmarid"'>Unmarid</option>
+                    <option value='Divorce"'>Divorce</option>
+                    <option value='Other"'>Ohter</option>
+                </Field>
+                <label>Blood Group</label>
+                <Field className="input"  name="blood" as="select">
+                     <option value='Group"'>Group</option>
+                    <option value='A+"'>A+</option>
+                    <option value='O+"'>O+</option>
+                    <option value='AB+"'>AB+</option>
+                    <option value='B+"'>B+</option>
+                </Field>
+                <br/>
+                <label>Nationality</label>
+                <Field className="input" name="nationality" type="text"></Field>
+               </div>
 
                 </div>
                 <br/>
